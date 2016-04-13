be356d6fbdb0e773da70d761cc730e754c65966f
Meutraa <paul@lost.host>
2016-04-13 16:28:19 +0200

    Began SDL init and window creation.

--- source/main.c
+++ source/main.c
@@ -4,13 +4,52 @@
 
 int main(int argc, char** argv)
 {
-    int init_return = SDL_Init(SDL_INIT_VIDEO|SDL_INIT_TIMER);
-    if(init_return != 0)
+    /* Ensures any return will call SDL_Quit first. */
+    atexit(SDL_Quit);
+    
+    /* Initialise the video subsystem. */
+    int iret = SDL_Init(SDL_INIT_VIDEO);
+    if(iret != 0)
     {
-        fprintf(stderr, "\nUnable to initialize SDL:  %s\n", SDL_GetError());
+        fprintf(stderr, "\nUnable to initialize SDL Video Subsystem:  %s\n", SDL_GetError());
         return 1;
     }
-    atexit(SDL_Quit);
-
+    
+    /* Initialise the timer subsystem. */
+    iret = SDL_InitSubSystem(SDL_INIT_TIMER);
+    if(iret != 0)
+    {
+        fprintf(stderr, "\nUnable to initialize SDL Timer Subsystem:  %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    /* Initialise the audio subsystem. */
+    iret = SDL_InitSubSystem(SDL_INIT_AUDIO);
+    if(iret != 0)
+    {
+        fprintf(stderr, "\nUnable to initialize SDL Audio Subsystem:  %s\n", SDL_GetError());
+        // Do not quit if audio fails
+    }
+    
+    /* Create window */
+    SDL_Window* window = SDL_CreateWindow(
+        "A Window",                 // Window title
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, // Position (x,y)
+        640, 480,                   // Size (x,y)
+        SDL_WINDOW_OPENGL
+        // Window flags http://wiki.libsdl.org/SDL_WindowFlags
+    );
+    
+    if(window == NULL)
+    {
+        fprintf(stderr, "\nCould not create window: %s\n", SDL_GetError());
+        return 1;
+    }
+    
+    SDL_Delay(3000);  // Pause execution for 3000 milliseconds, for example
+   
+    /* Clean up stuff. */
+    SDL_DestroyWindow(window);
+    
     return 0;
 }
