4242a1d0e2ccb05ddacdef54dedad2eb22c963fd
Meutraa <paul@lost.host>
2016-05-15 18:52:24 +0200

    Wireframe when pressing enter. Landscape 2d mesh with colors respresenting height.

--- include/macros.h
+++ include/macros.h
@@ -7,5 +7,21 @@
 
 #define SETEQ(a, b, c, d) if(!strcmp(a, b)) c = d;
 #define forXY(a, b) for(int y = a; y < b; y++) for(int x = a; x < b; x++)
+#define add_triangle(a, b, c) v[i++] = a.x; v[i++] = a.y; \
+                              v[i++] = b.x; v[i++] = b.y; \
+                              v[i++] = c.x; v[i++] = c.y;
+#define add_triangle_color(a, b, g) c[j++] = a; c[j++] = b; c[j++] = g; \
+                           c[j++] = a; c[j++] = b; c[j++] = g; \
+                           c[j++] = a; c[j++] = b; c[j++] = g;
+#define SCROLL(a, b, c, d, e) if(key_status[a] || (fullscreen && b == c)) \
+        { \
+            dx += scroll_speed * scale * d; \
+            dy += scroll_speed * scale * e; \
+            render = 1; \
+        }
+#define hxy heightmap[x][y]
+#define hx1y heightmap[x + 1][y]
+#define hxy1 heightmap[x][y + 1]
+#define h1xy heightmap[x + 1][y + 1]
 
 #endif
--- source/game.c
+++ source/game.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include <string.h>
 #include <time.h>
 
@@ -20,55 +21,24 @@
 
 static int event_loop();
 
-static const int maskmap[32] = {
-    0, 4, 5, 9, 6, 21, 10, 13, 7, 8, 20, 12, 11, 15, 14, [23] = 16, [27] = 17, [29] = 18, [30] = 19
-};
-static const int voffsetmap[32] = {
-    [2] = 1, [3] = 1, [6] = 1, [7] = 1, [10] = 1, [11] = 1, [14] = 1, [23] = 1, [29] = -1,
-};
-
-static const char* building_images[] = {
-    "resources/images/building-mega.tga",
-    "resources/images/building-1.tga",
-    "resources/images/building-2.tga",
-    "resources/images/building-2-1.tga",
-    "resources/images/building-2-2.tga",
-    "resources/images/building-2-3.tga",
-    "resources/images/building-2-4.tga",
-    NULL,
-};
-
 const char* layout = "resources/layouts/game_ui.csv";
-const char* terrain_path = "resources/images/terrains.png";
 
 /* Free up some space in the stack. */
-SDL_Texture **buildings;
 Drawable* drawables;
 SDL_Window* win;
 SDL_GLContext* con;
 SDL_Renderer* ren;
 int key_status[283];
 
-static inline int  LENGTH(void** array)  { int l = 0; while(array[l]) l++; return l; }
+static GLenum render_mode = GL_FILL;
 
 /* The grid of tiles. */
-static Tile tiles[GRID_SIZE][GRID_SIZE];
 static int heightmap[GRID_SIZE][GRID_SIZE];
 
 /* Tile dimensions must be divisible by exp2(DEFAULT_SCALE). */
 static const int TILE_WIDTH  = 64;
 static const int TILE_HEIGHT = 32;
 static const int TILE_DEPTH  = 8;
-
-static SDL_Rect src_rects[3][22];
-
-static SDL_Point pixel_to_tile(int x, int y)
-{
-    return (SDL_Point) {
-        (int) floor(((x / (float)TILE_WIDTH) + (3.0f * y / (float)TILE_HEIGHT / 2.0f)) - 0.5f),
-        (int) floor(((3.0f * y / (float)TILE_HEIGHT / 2.0f) - (x / (float)TILE_WIDTH)) + 0.5f)
-    };
-}
 
 static SDL_Point tile_to_pixel(int x, int y)
 {
@@ -78,69 +48,11 @@ static SDL_Point tile_to_pixel(int x, int y)
     };
 }
 
-/* Gives bound safe values for corner heights.
-   [0] = top, [1] = right, [2] = left, [3] = bottom */
-static void get_corner_heights(int heights[4], int x, int y)
-{
-    heights[0] = heightmap[x][y];
-    heights[1] = heightmap[x == GRID_SIZE - 1 ? x : x + 1][y];
-    heights[2] = heightmap[x][y == GRID_SIZE - 1 ? y : y + 1];
-    heights[3] = heightmap[x == GRID_SIZE - 1 ? x : x + 1][y == GRID_SIZE - 1 ? y : y + 1];
-}
-
-static void generate_map(void)
-{
-    fill_heightmap(heightmap, GRID_SIZE - 1, ROUGHNESS);
-
-    const int building_count = LENGTH((void**) building_images);
-
-    /* Create and fill the positions of the tiles. */
-    /* This loop runs upwards of one million times so take out constants! */
-    /* FIRST PASS */
-    forXY(0, GRID_SIZE)
-    {
-        SDL_Point pixel = tile_to_pixel(x , y);
-
-        int heights[4];
-        get_corner_heights(heights, x, y);
-        int u = heights[0], d = heights[3], l = heights[2], r = heights[1];
-
-        /* Get lowest corner. Loop deliberately unrolled.*/
-        int low = u;
-        if(d < low) low = d;
-        if(l < low) low = l;
-        if(r < low) low = r;
-
-        int ST = abs(u - d) >= 2 || abs(l - r) >= 2;
-        int mask = (l > low) | ((d > low) << 1) | ((r > low) << 2) | ((u > low) << 3) | (ST << 4);
-
-        Tile* tp = &tiles[x][y];
-        tp->x = pixel.x;
-        tp->y = pixel.y;
-        tp->voffset = (voffsetmap[mask] - heights[3]) * (int)(TILE_HEIGHT / 6.0f);
-        tp->water = u <= 0 || l <= 0 || r <= 0 || d <= 0;
-        tp->terrain_id = u + d + l + r >= 2 ? 0 : 2;
-        tp->tile_id  = !mask ? !u : maskmap[mask];
-        //tp->building = !tp->water && tp->tile_id == 0 && rand() % 6 == 0 ? buildings[rand() % building_count] : NULL;
-    }
-    /* SECOND PASS */
-    forXY(1, GRID_SIZE - 1)
-    {
-        /* \TODO if tile's corners are higher than tiles height, set as sand_grass. */
-        if(tiles[x - 1][y - 1].water + tiles[x - 1][y + 1].water + tiles[x + 1][y - 1].water + + tiles[x + 1][y + 1].water)
-        {
-            tiles[x][y].terrain_id = 2;
-        }
-    }
-    forXY(0, GRID_SIZE)
-    {
-        tiles[x][y].src = &src_rects[tiles[x][y].terrain_id][tiles[x][y].tile_id];
-    }
-}
-
 static GLuint vbo_id;
-static unsigned long land_count, water_count;
-#define vsize (12 * (GRID_SIZE ) * (GRID_SIZE))
+static unsigned long land_count;
+#define GRID_TOTAL (GRID_SIZE ) * (GRID_SIZE)
+#define vsize (12 * GRID_TOTAL)
+#define csize (18 * GRID_TOTAL)
 static GLfloat scale;
 float dx;
 float dy;
@@ -151,15 +63,14 @@ static void render_grid()
     glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
 
     glEnableClientState(GL_VERTEX_ARRAY);
+    glEnableClientState(GL_COLOR_ARRAY);
     glVertexPointer(2, GL_INT, 0, 0);
+    glColorPointer(3, GL_UNSIGNED_BYTE, 0, sizeof(GLint) * land_count);
 
-    glColor3ub(52, 171, 52);
-    glDrawArrays(GL_TRIANGLES, 0, land_count/2);
-
-    glColor3ub(102, 204, 255);
-    glDrawArrays(GL_TRIANGLES, land_count / 2, (water_count - land_count) / 2);
+    glDrawArrays(GL_TRIANGLES, 0, land_count >> 1);
 
     glDisableClientState(GL_VERTEX_ARRAY);
+    glDisableClientState(GL_COLOR_ARRAY);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     SDL_GL_SwapWindow(win);
 }
@@ -186,59 +97,82 @@ Status game_loop(SDL_Window* window, SDL_Renderer* renderer)
 
     srand((unsigned int)time(NULL));
 
-    /* Initialise globals. */
-    generate_map();
+    /* Create the map. */
+    fill_heightmap(heightmap, GRID_SIZE - 1, ROUGHNESS);
 
     glClearColor(0, 0, 0, 1);
+    //glShadeModel(GL_FLAT);
+    glDisable(GL_LIGHTING);
+    glDisable(GL_DEPTH_TEST);
+    glDisable(GL_DITHER);
+    glDisable(GL_TEXTURE_2D);
 
     /* Create vertex array. */
     GLint *v = calloc(vsize, sizeof(GLint));
-    unsigned long i = 0;
+    GLubyte *c = calloc(csize, sizeof(GLubyte));
+    int i = 0, j = 0;
+    land_count = 0;
     forXY(0, GRID_SIZE - 1)
     {
         SDL_Point p1 = tile_to_pixel(x, y);
         SDL_Point p2 = tile_to_pixel(x + 1, y);
         SDL_Point p3 = tile_to_pixel(x + 1, y + 1);
         SDL_Point p4 = tile_to_pixel(x, y + 1);
-        int h = TILE_DEPTH * heightmap[x][y];
 
-        if(tiles[x][y].tile_id == 0)
+        p1.y += TILE_DEPTH * hxy;
+        p2.y += TILE_DEPTH * hx1y;
+        p3.y += TILE_DEPTH * h1xy;
+        p4.y += TILE_DEPTH * hxy1;
+        /* where will the tile's color be split? */
+        if(p2.y != p4.y)
         {
-            if(h > 0)
-            {
-                v[i++] = p1.x; v[i++] = p1.y + h;
-                v[i++] = p2.x; v[i++] = p2.y + h;
-                v[i++] = p3.x; v[i++] = p3.y + h;
-                v[i++] = p3.x; v[i++] = p3.y + h;
-                v[i++] = p4.x; v[i++] = p4.y + h;
-                v[i++] = p1.x; v[i++] = p1.y + h;
-            }
+            add_triangle(p3, p4, p1)
+            add_triangle(p1, p2, p3)
+        }
+        else /* vertical split, or if not, the colors will be the same anyway. */
+        {
+            add_triangle(p1, p2, p4)
+            add_triangle(p4, p2, p3)
+        }
+        land_count += 12;
+        /* Add color vertices. */
+        //if(hxy == hx1y && hx1y == hxy1 && hxy1 == h1xy)
+        {
+            //add_triangle_color(82, 234, 82)
+            //add_triangle_color(82, 234, 82)
+            GLubyte p = (hxy + 24) * 2;
+            add_triangle_color(p, p, p);
+            add_triangle_color(p, p, p);
+        }
+        /*else if(4 == tiles[x][y].tile_id)
+        {
+            add_triangle_color(94, 255, 94);
+            add_triangle_color(80, 228, 80);
+        }
+        else if(18 == tiles[x][y].tile_id)
+        {
+            add_triangle_color(63, 187, 63);
+            add_triangle_color(63, 187, 63);
+        }*/
+        //else
+        {
+            //add_triangle_color(64, 64, 64)
+            //add_triangle_color(64, 64, 64)
         }
     }
-    land_count = i;
-    forXY(0, GRID_SIZE - 1)
-    {
-        SDL_Point p1 = tile_to_pixel(x, y);
-        SDL_Point p2 = tile_to_pixel(x + 1, y);
-        SDL_Point p3 = tile_to_pixel(x + 1, y + 1);
-        SDL_Point p4 = tile_to_pixel(x, y + 1);
 
-            if(heightmap[x][y] <= 0)
-            {
-                v[i++] = p1.x; v[i++] = p1.y;
-                v[i++] = p2.x; v[i++] = p2.y;
-                v[i++] = p3.x; v[i++] = p3.y;
-                v[i++] = p3.x; v[i++] = p3.y;
-                v[i++] = p4.x; v[i++] = p4.y;
-                v[i++] = p1.x; v[i++] = p1.y;
-            }
-    }
-
-    water_count = i;
     glGenBuffers(1, &vbo_id);
     glBindBuffer(GL_ARRAY_BUFFER, vbo_id);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(GLint) * i, v, GL_STATIC_DRAW);
+
+    unsigned long all_size = (vsize * sizeof(GLint)) + (csize * sizeof(GLubyte));
+    GLint *all = malloc(all_size);
+    memmove(all, v, vsize * sizeof(GLint));
     free(v);
+    memmove(all + vsize, c, csize * sizeof(GLubyte));
+    free(c);
+
+    glBufferData(GL_ARRAY_BUFFER, all_size, all, GL_STATIC_DRAW);
+    free(all);
 
     int status = 1;
     render_grid();
@@ -246,10 +180,6 @@ Status game_loop(SDL_Window* window, SDL_Renderer* renderer)
     {
         status = event_loop();
         int render = 0;
-    #define SCROLL(a, b, c, d, e) if(key_status[a] || (fullscreen && b == c)) \
-        { dx += scroll_speed * scale * d; \
-          dy += scroll_speed * scale * e; \
-          render = 1; }
         SCROLL(SDL_SCANCODE_A, 0,    100,  -1.0f,  0.0f)
         SCROLL(SDL_SCANCODE_D, 1279, 100,  1.0f,  0.0f)
         SCROLL(SDL_SCANCODE_W, 0,    100,  0.0f, 0.5f)
@@ -273,6 +203,13 @@ static int event_loop()
         {
             key_status[event.key.keysym.scancode] = event.type == SDL_KEYUP ? 0 : 1;
             if(key_status[41]) return 0;
+            if(SDL_SCANCODE_RETURN == event.key.keysym.scancode
+                && SDL_KEYDOWN == event.key.type && 0 == event.key.repeat)
+            {
+                render_mode = render_mode == GL_FILL ? GL_LINE : GL_FILL;
+                glPolygonMode(GL_FRONT_AND_BACK, render_mode);
+                render_grid();
+            }
         }
         else if(SDL_MOUSEWHEEL == event.type)
         {
