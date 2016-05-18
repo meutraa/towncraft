01a39759e01286d41ac87e0a38fccba49253e399
Meutraa <paul@lost.host>
2016-05-18 03:31:48 +0200

    Made heightmap generation about twice as fast.

--- source/diamond.c
+++ source/diamond.c
@@ -1,52 +1,49 @@
 #include "diamond.h"
 
 #include <stdlib.h>
-#include <math.h>
 
-#define HEIGHT 18
-#define LOWER_HEIGHT 9
-const float ROUGHNESS = 0.033f;
-
-static int add(int x, int y, int height[GRID_SIZE][GRID_SIZE], int* c)
-{
-    if(x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE)
-    {
-        (*c)++;
-        return height[x][y];
-    }
-    return 0;
-}
+static const int HEIGHT = 18;
+static const int LOWER_HEIGHT = 9;
+const float ROUGHNESS = 0.1f;
+#define ROUND(x) ((x) >= 0 ? (int)((x) + 0.5f) : (int)((x) - 0.5f))
 
 static void diamond_square(int height[GRID_SIZE][GRID_SIZE], int size, const float roughness)
 {
-    const int half = size >> 1;
+    int half = size >> 1, ymh, yph, xmh, xph;
+    int yl, yh, xl, xh;
     if (half < 1) return;
 
     int x, y, c, total;
-    const float scale = roughness * size;
+    const float A = (roughness * size) / (2.0f * RAND_MAX);
+    const float B = RAND_MAX >> 1;
 
     for(y = half; y < GRID_SIZE - 1; y += size)
     {
+        ymh = y - half , yph = y + half;
+        yl = ymh >= 0, yh = yph < GRID_SIZE;
         for(x = half; x < GRID_SIZE - 1; x += size)
         {
-            c = 0;
-            total = add(x - half, y - half, height, &c)
-                  + add(x + half, y - half, height, &c)
-                  + add(x + half, y + half, height, &c)
-                  + add(x - half, y + half, height, &c);
-            height[x][y] = (int) round(((float)total / c) + ((rand()/(float)RAND_MAX * scale * 2.0f) - scale));
+            total = 0, c = 0, xmh = x - half, xph = x + half;
+            xl = xmh >= 0, xh = xph < GRID_SIZE;
+            if(xl && yl) { c++; total += height[xmh][ymh]; }
+            if(xh && yl) { c++; total += height[xph][ymh]; }
+            if(xh && yh) { c++; total += height[xph][yph]; }
+            if(xl && yh) { c++; total += height[xmh][yph]; }
+
+            height[x][y] = ROUND(((float)total / c) + (A * (rand() - B)));
         }
     }
     for(y = 0; y < GRID_SIZE; y += half)
     {
+        ymh = y - half, yph = y + half;
         for(x = (y + half) % size; x < GRID_SIZE; x += size)
         {
-            c = 0;
-            total = add(x, y - half, height, &c)
-                  + add(x + half, y, height, &c)
-                  + add(x, y + half, height, &c)
-                  + add(x - half, y, height, &c);
-            height[x][y] = (int) round(((float)total / c) + ((rand()/(float)RAND_MAX * scale * 2.0f) - scale));
+            total = 0, c = 0, xmh = x - half, xph = x + half;
+            if(ymh >= 0)        { c++; total += height[x][ymh]; }
+            if(xph < GRID_SIZE) { c++; total += height[xph][y]; }
+            if(yph < GRID_SIZE) { c++; total += height[x][yph]; }
+            if(xmh >= 0)        { c++; total += height[xmh][y]; }
+            height[x][y] = ROUND(((float)total / c) + (A * (rand() - B)));
         }
     }
     diamond_square(height, size >> 1, roughness);
